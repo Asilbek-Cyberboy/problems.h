ac11009edabc81eb3bcbe5a34226b14e6a6760c3
Asilbek-Cyberboy <a.temirov@newuu.uz>
2024-11-03 10:15:03 +0500

    Update problems.h

--- problems.h
+++ problems.h
@@ -1,9 +1,7 @@
 //In the comments after each problem functions' names are provided. Your task to define correct datatype and parameters.
 //DO NOT CHANGE FUNCTIONS' NAMES.
 //You may check your functions in main.cpp as shown by exampleFunction;
-void exampleFunction() {
-    std::cout<<"Example Function that can be called in main."<<std::endl;
-}
+
 // Problem 1
 // floorFunction
 // ceilingFunction
