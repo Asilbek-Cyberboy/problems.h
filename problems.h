824889f7b78135c4f184e973b1f0c22c5b91f144
Asilbek-Cyberboy <a.temirov@newuu.uz>
2024-11-03 10:12:13 +0500

    Update problems.h

--- problems.h
+++ problems.h
@@ -1,8 +1,6 @@
 //In the comments after each problem functions' names are provided. Your task to define correct datatype and parameters.
 //DO NOT CHANGE FUNCTIONS' NAMES.
 //You may check your functions in main.cpp as shown by exampleFunction;
-#include <cmath>
-using namespace std;
 void exampleFunction() {
     std::cout<<"Example Function that can be called in main."<<std::endl;
 }
